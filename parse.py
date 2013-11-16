8c593cbf7c6e8d19061c2875cf936c0ea396c79c
Jonathan Reeve <jon.reeve@gmail.com>
2013-11-16 17:11:06 -0500

    cleaned up

--- parse.py
+++ parse.py
@@ -13,16 +13,12 @@ import sys # library for doing system things
 from optparse import OptionParser #parse options from the command line!
 from lxml import etree # for parsing XML
 import re # need regex for doing fancy split
-import logging # send debugging messages
 
-# Options {{{1
 parser = OptionParser('usage: %prog [options] file1.xml [file2.xml]') 
 parser.add_option("-x", "--xpath", action="store", dest="xpath",         
 		help="A text file containing a comma-separated list of the names (i.e. XML IDs) of the characters whose dialog you want to extract. Default: characters.txt")
 parser.add_option("-v", "--verbose", action="store_true", dest="verbose")
 parser.add_option("-q", "--quiet", action="store_false", dest="verbose")
-
-# End of Options }}} 
 
 (options, files) = parser.parse_args()
 
@@ -41,7 +37,6 @@ for xpath in xpath:
 	xpaths.append(xpath.strip()) #remove whitespace
 xpaths=filter(None,xpaths) # get rid of empty entries
 
-#Verbose output will help to debug
 if options.verbose:
         print "Using xpath(s): "
         for xpath in xpaths:
@@ -67,7 +62,6 @@ xmls = [rawxml.replace(' xmlns="http://www.tei-c.org/ns/1.0"','') for rawxml in 
 if options.verbose: 
     print "Here's what the beginning of the first XML file looks like after stripping out the namespace:\n" + xmls[0][:100] 
 
-#load xml files
 xmls=[etree.fromstring(xmlfile) for xmlfile in xmls] #parse files 
 
 #from suggestion here: http://stackoverflow.com/questions/16640041/what-is-an-elementtree-object-exactly-and-how-can-i-get-data-from-it?noredirect=1#16640278 
@@ -77,17 +71,7 @@ for xml in xmls:
  	for xpath in xpaths: 
 		output+=xml.xpath(xpath + '/text()')
 
-#print "Output: " 
-#print output
-
-#exit() #temporary breakpoint
-
 clean=""
-#new way 
 clean = '\t'.join(line.strip() for line in output)
-#old way
-#for line in output: 
-#	clean+=line.strip()+'\n' #strip all extra space and then make them individual lines
 
-#encoding it makes it work with piping as described here: http://stackoverflow.com/questions/492483/setting-the-correct-encoding-when-piping-stdout-in-python 
 encoded=sys.stdout.write(clean.encode('utf-8'))
